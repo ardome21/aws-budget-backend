406b935285416477d5dab85b587efb4553dd07c5
ardome21 <ardome21@gmail.com>
2025-08-08 19:25:58 -0400

    dyn

--- .aws/terraform/impl/dynamodb/outputs.tf
+++ .aws/terraform/impl/dynamodb/outputs.tf
@@ -1,9 +1,9 @@
 output "user_table_name" {
   description = "The name of the DynamoDB table"
-  value       = aws_dynamodb_table.user_table.name
+  value       = aws_dynamodb_table.this.name
 }
 
 output "user_table_arn" {
   description = "The ARN of the DynamoDB table"
-  value       = aws_dynamodb_table.user_table.arn
+  value       = aws_dynamodb_table.this.arn
 }
