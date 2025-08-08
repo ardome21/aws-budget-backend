0f599e2d20af40b125241f679e4cfd5f89f8fb07
ardome21 <ardome21@gmail.com>
2025-08-08 15:19:29 -0400

    pink

--- .aws/terraform/outputs.tf
+++ .aws/terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "api_gateway_full_url" {
-  description = "Full URL of the API Gateway"
-  value       = aws_apigatewayv2_stage.default.invoke_url
-}
-
 output "jwt_layer_arn" {
   description = "ARN of the jwt layer"
   value       = aws_lambda_layer_version.jwt_layer.arn
