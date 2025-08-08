7c75bff51fbac59f10fc16f210e96b04458b3f80
ardome21 <ardome21@gmail.com>
2025-08-08 14:45:43 -0400

    try to fix

--- .aws/terraform/impl/lambda/main.tf
+++ .aws/terraform/impl/lambda/main.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "this" {
   function_name = var.lambda_name
   role          = var.existing_iam_role_arn
-  handler       = var.handler
+  handler       = 'main.lambda_handler'
   runtime       = "python3.11"
   filename      = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
--- .aws/terraform/impl/lambda/outputs.tf
+++ .aws/terraform/impl/lambda/outputs.tf
@@ -1,14 +1,14 @@
 output "lambda_function_arn" {
   description = "ARN of the Lambda function"
-  value       = aws_lambda_function.template_lambda.arn
+  value       = aws_lambda_function.login_lambda.arn
 }
 
 output "function_name" {
   description = "Name of the Lambda function"
-  value       = aws_lambda_function.template_lambda.function_name
+  value       = aws_lambda_function.login_lambda.function_name
 }
 
 output "invoke_arn" {
   description = "Invoke ARN of the Lambda function for API Gateway"
-  value       = aws_lambda_function.template_lambda.invoke_arn
+  value       = aws_lambda_function.login_lambda.invoke_arn
 }
