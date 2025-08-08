48910b61959f36e78f7b5161ef468ed321bfe8fd
ardome21 <ardome21@gmail.com>
2025-08-08 14:04:45 -0400

    uh

--- .aws/terraform/impl/lambda/main.tf
+++ .aws/terraform/impl/lambda/main.tf
@@ -9,14 +9,10 @@ variable "lambda_layers" {
 resource "aws_lambda_function" "template_lambda" {
   function_name = var.lambda_name
   role          = var.existing_iam_role_arn
-  handler       = "main.lambda_hander"
+  handler       = "main.lambda_handler"
   runtime       = "python3.11"
-  filename      = var.lamdba_filename
+  filename      = var.lambda_filename
   layers        = var.lambda_layers
   timeout       = 30
   memory_size   = 128
-
-  tags = {
-    Environment = "dev"
-  }
 }
