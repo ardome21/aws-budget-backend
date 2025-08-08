66b0c3adb9c989128da41cefe18daefbb4e84a21
ardome21 <ardome21@gmail.com>
2025-08-08 14:48:07 -0400

    module

--- .aws/terraform/impl/lambda/outputs.tf
+++ .aws/terraform/impl/lambda/outputs.tf
@@ -1,14 +1,14 @@
 output "lambda_function_arn" {
   description = "ARN of the Lambda function"
-  value       = aws_lambda_function.login_lambda.arn
+  value       = module.login_lambda.arn
 }
 
 output "function_name" {
   description = "Name of the Lambda function"
-  value       = aws_lambda_function.login_lambda.function_name
+  value       = module.login_lambda.function_name
 }
 
 output "invoke_arn" {
   description = "Invoke ARN of the Lambda function for API Gateway"
-  value       = aws_lambda_function.login_lambda.invoke_arn
+  value       = module.login_lambda.invoke_arn
 }
