c98516986d9d92bf169f2203c6dd588c6a745a03
ardome21 <ardome21@gmail.com>
2025-08-08 00:37:53 -0400

    bad bad

--- .aws/terraform/backend.tf
+++ .aws/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "budget-terraform-state-dev"
     key            = "lambda-api/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-1- bad bad"
     encrypt        = true
     dynamodb_table = "budget-terraform-state-lock"
   }
