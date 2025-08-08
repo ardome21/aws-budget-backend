e05632eaca5ba5dc1cfafa32844c09bf8ecfd9e9
ardome21 <ardome21@gmail.com>
2025-08-08 13:58:33 -0400

    main

--- .aws/terraform/lambda.tf
+++ .aws/terraform/lambda.tf
@@ -1,7 +1,7 @@
 module "login_lambda" {
   source                = "./impl/lambda"
   lambda_name           = "login_lambda"
-  lambda_filename       = "../../lambdas/auth/Login/lambda_function.py"
+  lambda_filename       = "../../lambdas/auth/Login/main.py"
   lambda_layers         = [aws_lambda_layer_version.jwt_layer.arn]
   existing_iam_role_arn = var.existing_iam_role_arn
   }
