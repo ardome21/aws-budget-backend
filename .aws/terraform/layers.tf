0056d61d983ea8ce600ccfa7203edcbac12f0066
ardome21 <ardome21@gmail.com>
2025-08-08 11:04:59 -0400

    other path

--- .aws/terraform/layers.tf
+++ .aws/terraform/layers.tf
@@ -3,5 +3,5 @@ resource "aws_lambda_layer_version" "bcrypt_layer" {
   description         = "Bcrypt library for password hashing"
   compatible_runtimes = ["python3.11"]
   filename            = "../../layers/bcrypt.zip"  # Adjust path based on your repo structure
-  source_code_hash = filebase64sha256("../layers/bcrypt.zip")
+  source_code_hash = filebase64sha256("../../layers/bcrypt.zip")
 }
