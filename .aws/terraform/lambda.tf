34c0e9b3a7611fdc651088544f6d7c4a32f06f1d
ardome21 <ardome21@gmail.com>
2025-08-08 13:43:17 -0400

    folder

--- .aws/terraform/lambda.tf
+++ .aws/terraform/lambda.tf
@@ -1,5 +1,5 @@
 module "login_lambda" {
-  source          = "./impl/lambda.tf"
+  source          = "./impl/lambda"
   lambda_name     = "login_lambda"
   lambda_handler  = "login.handler"
   lambda_filename = "login.zip"
