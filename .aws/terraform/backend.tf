22692a594719f128176a70ed106b145ff5fcbe19
ardome21 <ardome21@gmail.com>
2025-08-08 00:34:53 -0400

    oh

--- .aws/terraform/backend.tf
+++ .aws/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "budget-terraform-state"
+    bucket         = "budget-terraform-state-dev"
     key            = "lambda-api/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
