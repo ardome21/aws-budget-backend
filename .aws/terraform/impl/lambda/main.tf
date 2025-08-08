50216aaf7d73c0800f482b6b2c4089bc3b33fdf6
ardome21 <ardome21@gmail.com>
2025-08-08 14:33:31 -0400

    woohoo?

--- .aws/terraform/impl/lambda/main.tf
+++ .aws/terraform/impl/lambda/main.tf
@@ -1,9 +1,17 @@
-resource "aws_lambda_function" "template_lambda" {
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = var.source_file
+  output_path = "${path.module}/output/${var.lambda_name}.zip"
+}
+
+resource "aws_lambda_function" "this" {
   function_name = var.lambda_name
   role          = var.existing_iam_role_arn
-  handler       = "main.lambda_handler"
+  handler       = var.handler
   runtime       = "python3.11"
-  filename      = var.lambda_filename
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
   layers        = var.lambda_layers
   timeout       = 30
   memory_size   = 128
