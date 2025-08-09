9446098df3d0a9fa322f7ce28820ca424a34adf7
ardome21 <ardome21@gmail.com>
2025-08-08 20:02:58 -0400

    create_account

--- .aws/terraform/lambda.tf
+++ .aws/terraform/lambda.tf
@@ -22,10 +22,10 @@
   lambda_layers         = [aws_lambda_layer_version.jwt_layer.arn]
 }
 
-  module "sign_up_lambda" {
+  module "create_account_lambda" {
   source               = "./impl/lambda"
-  lambda_name          = "sign-up-account-dev"
-  source_file          = "../../lambdas/auth/Signup/main.py"
+  lambda_name          = "create-account-dev"
+  source_file          = "../../lambdas/auth/CreateAccount/main.py"
   existing_iam_role_arn = var.existing_iam_role_arn
   lambda_layers         = [aws_lambda_layer_version.jwt_layer.arn]
 }
