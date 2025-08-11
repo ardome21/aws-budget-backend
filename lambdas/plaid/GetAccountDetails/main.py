2930e066506ecd561996ae577c95159d9b96d6fa
ardome21 <ardome21@gmail.com>
2025-08-11 00:50:50 -0400

    get account details but not wiht verified login

--- lambdas/plaid/GetAccountDetails/main.py
+++ lambdas/plaid/GetAccountDetails/main.py
@@ -48,20 +48,27 @@ def get_auth_token(event):
 
 def get_access_token(user_id, institution):
 
-    response = userTable.query(
+    response = accessTable.query(
         KeyConditionExpression=Key('user_id').eq(user_id),
         FilterExpression=Attr('institution_name').eq(institution)
     )
+    print(f'User: {response}')
     item = response['Items'][0]
     print('Got access token')
     encrypted_access_token = item['access_token']
+    print(f'Access token: {encrypted_access_token}')
     jwt_secret = boto3.client('ssm').get_parameter(Name='/budget/jwt-secret-key', WithDecryption=True)['Parameter']['Value']
     decrypted_access_token = jwt.decode(encrypted_access_token, jwt_secret, algorithms=['HS256'])
-    return decrypted_access_token
+    return decrypted_access_token['access_token']
 
 
 def lambda_handler(event, context):
     try:
+        http_method = event.get('httpMethod') or event.get(
+            'requestContext', {}).get('http', {}).get('method')
+        if http_method == 'OPTIONS':
+            print("Handling OPTIONS preflight request")
+            return
         ssm_client = boto3.client('ssm')
         client_id = ssm_client.get_parameter(Name='/budget/plaid/client_id', WithDecryption=True)['Parameter']['Value']
         sandbox_secret = ssm_client.get_parameter(Name='/budget/plaid/sandbox_secret', WithDecryption=True)['Parameter']['Value']
@@ -76,10 +83,12 @@ def lambda_handler(event, context):
         api_client = ApiClient(configuration)
         client = plaid_api.PlaidApi(api_client)
         
+        print(f'Event: {event}')
         if isinstance(event['body'], str):
             body = json.loads(event['body'])
         else:
             body = event['body']
+        print(f'Body: {body}')
         user_id = body['user_id']
         institution = body['institution']
         # verify_auth(user_id, event)
